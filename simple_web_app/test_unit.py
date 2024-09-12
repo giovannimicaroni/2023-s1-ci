195bc6a620f56f825e3d7e204c42d27f0eec8b84
giovannimicaroni <gimicaroni@gmail.com>
2024-09-12 09:21:39 -0300

    testes adicionados

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -67,3 +67,24 @@ def test_8_chars_with_no_upper_case_password_should_raise_exception():
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
+def test_8_chars_with_slash_especial_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eA/B1eA")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_tilde_especial_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eA~B1eA")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_caret_especial_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eA^B1eA")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
